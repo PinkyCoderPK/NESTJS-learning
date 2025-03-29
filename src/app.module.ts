efcbc36b48cac6cdfb0fdfcb08396faf5a8aa72a
Pinky Coder <pinksama69@gmail.com>
2025-03-29 15:27:04 +0700

    MongoDB Connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://NestJS_Test:PQz5kqZBgrkDdrC8@pkdtb.ghmka.mongodb.net/',
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
